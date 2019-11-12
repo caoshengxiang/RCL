ce54aa13cff15dc1703715e933abaac1a65b1475
caoxiangxiang <1104729815@qq.com>
2019-11-12 19:33:30 +0800

    船期爬虫

---
+++ RCL/items.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class PortItem(scrapy.Item):
+    # define the fields for your item here like:
+    portCode = scrapy.Field()
+    port = scrapy.Field()
+    date = scrapy.Field()
+
+class GroupItem(scrapy.Item):
+    # define the fields for your item here like:
+    pol = scrapy.Field()
+    pod = scrapy.Field()
+    polName = scrapy.Field()
+    podName = scrapy.Field()
+    useTime = scrapy.Field()
+    date = scrapy.Field()
+    IS_TRANSIT = scrapy.Field()
+    VESSEL = scrapy.Field()
+    VOYAGE = scrapy.Field()
+    POL_NAME_EN = scrapy.Field()
+    LPA = scrapy.Field()
+    ETD = scrapy.Field()
+    POD_NAME_EN = scrapy.Field()
+    ETA = scrapy.Field()
+    TRANSIT_TIME = scrapy.Field()
+    FLAG = scrapy.Field()
+    TransferCode = scrapy.Field()
+    TransferName = scrapy.Field()
---
+++ RCL/spiders/meiju.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class MeijuSpider(scrapy.Spider):
+    name = 'meiju'
+    allowed_domains = ['meijutt.com']
+    start_urls = ['http://www.meijutt.com/new100.html']
+
+    def parse(self, response):
+        print(response.text)
+        pass
