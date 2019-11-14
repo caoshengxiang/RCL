78d40087c8534879f8724d2f43abaed150342cf2
liqiang <1144388620@qq.com>
2019-11-14 12:10:16 +0800

    fix

--- RCL/settings.py
+++ RCL/settings.py
@@ -33,7 +33,7 @@ LOG_STDOUT = True
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -42,8 +42,8 @@ RANDOMIZE_DOWNLOAD_DELAY = True
 DOWNLOAD_DELAY = 0.25
 
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 16  # 默认： 8 将对任何单个域执行的并发（即，并发）请求的最大数量。
-CONCURRENT_REQUESTS_PER_IP = 16  # 默认： 0,将对任何单个IP执行的并发（即，并发）请求的最大数量
+CONCURRENT_REQUESTS_PER_DOMAIN = 1  # 默认： 8 将对任何单个域执行的并发（即，并发）请求的最大数量。
+CONCURRENT_REQUESTS_PER_IP = 1  # 默认： 0,将对任何单个IP执行的并发（即，并发）请求的最大数量
 
 RETRY_ENABLED = True  # 是否开启retry
 RETRY_TIMES = 3  # 重试次数
