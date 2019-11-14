fd1d6c1dc2dada613b00bfaa04f0b8c5bdd25ebc
liqiang <1144388620@qq.com>
2019-11-14 18:08:38 +0800

    fix

--- RCL/settings.py
+++ RCL/settings.py
@@ -35,6 +35,7 @@ ROBOTSTXT_OBEY = False
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 16
 
+
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
