b828296a367f9c049ff435721cd1d7fbe9497e56
liqiang <1144388620@qq.com>
2019-12-03 15:27:09 +0800

    fix

---
+++ test/test2.py
@@ -0,0 +1,12 @@
+# -*- coding utf-8 -*-#
+# ------------------------------------------------------------------
+# Name:      test2
+# Author:    liangbaikai
+# Date:      2019/12/3
+# Desc:      there is a python file description
+# ------------------------------------------------------------------
+
+if __name__ == '__main__':
+    x=dict(a=1)
+    x.setdefault('1',2)
+    print(x)
