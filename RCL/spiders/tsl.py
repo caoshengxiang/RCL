e94dab66843af41de996d60d91ce1acc9bdf2bb3
liqiang <1144388620@qq.com>
2019-12-09 11:13:56 +0800

    fix

--- RCL/spiders/tsl.py
+++ RCL/spiders/tsl.py
@@ -30,7 +30,7 @@ class TslSpider(scrapy.Spider):
 
     custom_settings = {  # 指定配置的通道, 要对应找到每个爬虫指定的管道,settings里也要进行管道配置
         'ITEM_PIPELINES': {
-            'RCL.pipelines.MongoPipeline': 300
+            'RCL.pipelines.MysqlPipeline': 300
         }
     }
 
@@ -51,9 +51,16 @@ class TslSpider(scrapy.Spider):
 
     def __init__(self):
         chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         No_Image_loading = {"profile.managed_default_content_settings.images": 2}
         chrome_options.add_experimental_option("prefs", No_Image_loading)
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        # chrome_options.binary_location = r"D:\soft\googlechrome\Application\77.0.3865.120\chrome.exe"
+        # epath = "D:/work/chromedriver.exe"
+        epath = "/usr/bin/chromedriver"
+        self.driver = webdriver.Chrome(executable_path=epath,chrome_options=chrome_options)
         # 设置超时，双重保险,设置后derver 就死了
         self.driver.set_page_load_timeout(30)
         self.driver.set_script_timeout(30)
@@ -112,7 +119,7 @@ class TslSpider(scrapy.Spider):
                     logging.error(e)
 
                 for o_h in o_h_c:
-                    # # 测试
+                    # 测试
                     # if o_h['name'] != 'HONGKONG':
                     #     continue
                     try:
@@ -208,3 +215,15 @@ class TslSpider(scrapy.Spider):
                                 logging.error(e)
                     except Exception as e:
                         continue
+
+    @staticmethod
+    def close(spider, reason):
+        """
+        关闭chrome
+        :param spider:
+        :param reason:
+        :return:
+        """
+        if spider.driver:
+            spider.driver.quit()
+        super().close(spider, reason)
