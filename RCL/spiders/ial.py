ca3600e0253b7cb6fc5b6ef7adab8d415692a783
caoxiangxiang <1104729815@qq.com>
2019-12-13 12:46:40 +0800

    se

--- RCL/spiders/ial.py
+++ RCL/spiders/ial.py
@@ -33,8 +33,8 @@ class IalSpider(scrapy.Spider):
     start_urls = ['http://www.interasia.cc/content/c_service/sailing_schedule.aspx?SiteID=1']
     custom_settings = {  # 指定配置的通道, 要对应找到每个爬虫指定的管道,settings里也要进行管道配置
         'ITEM_PIPELINES': {
-            'RCL.pipelines.MongoPipeline': 300
-            # 'RCL.pipelines.MysqlPipeline': 300
+            # 'RCL.pipelines.MongoPipeline': 300
+            'RCL.pipelines.MysqlPipeline': 300
         }
     }
 
